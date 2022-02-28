64d1a6fed0eb78a5a1590117177f99eeb9926a01
Balogun Olalere <lordareello@gmail.com>
2022-02-28 10:34:47 +0100

    update user.py

--- app/api/api_v1/endpoints/user.py
+++ app/api/api_v1/endpoints/user.py
@@ -1,20 +1,24 @@
 from fastapi import APIRouter
+from models.user_model import *
+from services.user_services import *
 
 router = APIRouter()
 
 
 @router.get('/')
-async def search_username():
-    return username
+async def search_username(user : User):
+    resp = search_user(**user)
+    return resp
 
 @router.get('/profile/')
-async def get_user_profile():
-    return profile
+async def get_user_profile(profile: UserProfile):
+    resp = get_profile(**profile)
+    return resp
 
-@router.get('/{}/followers')
-async def get_user_followers():
-    return followers
+# @router.get('/{}/followers')
+# async def get_user_followers():
+#     return followers
 
-@router.get('/{}/following')
-async def get_following():
-    return following
+# @router.get('/{}/following')
+# async def get_following():
+#     return following
